3b9a63e1e4d2162e222b7163e12032b0b9a785e1
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-08-30 10:21:06 +0100

    initial commit

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,37 @@
     $('.modal').modal();
     $(".button-collapse").sideNav();
     $('.tooltipped').tooltip({delay: 500});
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        hover: true, // Activate on hover
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'right' // Displays dropdown with edge aligned to the left of button
+      }
+    );
     
-  });
+    
+ });
  
+ $.validator.setDefaults({
+    errorClass: 'invalid',
+    validClass: "valid",
+    errorPlacement: function (error, element) {
+        $(element)
+            .closest("form")
+            .find("label[for='" + element.attr("id") + "']")
+            .attr('data-error', error.text());
+    },
+    submitHandler: function (form) {
+        console.log('form ok');
+    }
+});
+
+$(".former").validate({
+    rules: {
+       
+    }
+});
 
 window.onscroll = function() {scrollFunction()};
 
